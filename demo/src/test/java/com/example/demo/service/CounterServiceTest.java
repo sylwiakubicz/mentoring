ce4d8e017ee19b684581378b011cee799d0b436a
sylwiakubicz <sylwiakubicz31@gmail.com>
2026-04-17 12:07:37 +0200

    add tests for counter service

--- demo/src/test/java/com/example/demo/service/CounterServiceTest.java
+++ demo/src/test/java/com/example/demo/service/CounterServiceTest.java
@@ -1,4 +1,21 @@
 package com.example.demo.service;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CounterServiceTest {
+    @Test
+    void shoulCreateBasicCounterWhenNotPremium() {
+        CounterService service = new CounterService(false);
+        service.acceptText("Hello hello");
+        assertThat(service.getCount("hello")).isEqualTo(2);
+    }
+
+    @Test
+    void shouldCreatePremiumCounterWhenPremium() {
+        CounterService service = new CounterService(true);
+        service.acceptText("Hello hello");
+        assertThat(service.getCount("Hello")).isEqualTo(1);
+        assertThat(service.getCount("hello")).isEqualTo(1);
+    }
 }
